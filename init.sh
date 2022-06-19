310a66021cb81880ab88deaa67c8b346b9b6c322
Ben Sykes <hootieben@gmail.com>
2022-06-19 08:54:21 -0500

    Clone workstation repo and install ansible requirements

--- init.sh
+++ init.sh
@@ -7,3 +7,5 @@ sudo add-apt-repository --yes --update ppa:git-core/ppa
 sudo apt-get install --yes git software-properties-common python-is-python3 python3-pip kitty-terminfo
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt-get install --yes ansible
+git clone -b v3 https://github.com/hootieben/workstation.git ~/.config/workstation
+ansible-galaxy install -r ~/.config/workstation/requirements.yml
