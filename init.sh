9684f9d2a15ce7581c2497eaed8353d236fefa8b
Ben Sykes <hootieben@gmail.com>
2022-06-19 10:23:33 -0500

    Split init.sh to only run privileged actions

--- init.sh
+++ init.sh
@@ -3,9 +3,7 @@
 set -exu
 
 export DEBIAN_FRONTEND=noninteractive
-sudo add-apt-repository --yes --update ppa:git-core/ppa
-sudo apt-get install --yes git software-properties-common python-is-python3 python3-pip kitty-terminfo
-sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt-get install --yes ansible
-git clone -b v3 https://github.com/hootieben/workstation.git ~/.config/workstation
-ansible-galaxy install -r ~/.config/workstation/requirements.yml
+add-apt-repository --yes --update ppa:git-core/ppa
+apt-get install --yes git software-properties-common python-is-python3 python3-pip kitty-terminfo
+add-apt-repository --yes --update ppa:ansible/ansible
+apt-get install --yes ansible
