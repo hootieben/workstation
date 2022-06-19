52017d39299c89396a7109775ba0692e039e35aa
Ben Sykes <hootieben@gmail.com>
2022-06-19 08:52:25 -0500

    Run vagrant init as unprivileged user

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@
 set -exu
 
 export DEBIAN_FRONTEND=noninteractive
-add-apt-repository --yes --update ppa:git-core/ppa
-apt-get install --yes git software-properties-common python-is-python3 python3-pip kitty-terminfo
-add-apt-repository --yes --update ppa:ansible/ansible
-apt-get install --yes ansible
+sudo add-apt-repository --yes --update ppa:git-core/ppa
+sudo apt-get install --yes git software-properties-common python-is-python3 python3-pip kitty-terminfo
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt-get install --yes ansible
