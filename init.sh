4828b65830d94f528496145d5ae62e3befa8e551
Ben Sykes <hootieben@gmail.com>
2022-06-19 08:24:12 -0500

    Adding kitty-terminfo package as required for testing

--- init.sh
+++ init.sh
@@ -4,6 +4,6 @@ set -exu
 
 export DEBIAN_FRONTEND=noninteractive
 add-apt-repository --yes --update ppa:git-core/ppa
-apt-get install --yes git software-properties-common python-is-python3 python3-pip
+apt-get install --yes git software-properties-common python-is-python3 python3-pip kitty-terminfo
 add-apt-repository --yes --update ppa:ansible/ansible
 apt-get install --yes ansible
