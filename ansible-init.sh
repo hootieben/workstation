c2e5550ae83b1bf85380428c01a878c294719db6
Ben Sykes <hootieben@gmail.com>
2022-06-23 10:21:08 -0500

    Remove extraneous conditionals for branch checkout

--- ansible-init.sh
+++ ansible-init.sh
@@ -7,12 +7,6 @@ GITBRANCH="${1:-$DEFAULTBRANCH}"
 REPOSRC=https://github.com/hootieben/workstation.git
 LOCALREPO=~/.config/workstation
 
-# if [ ! -d $LOCALREPO ]
-# then
-#   git clone -b $GITBRANCH $REPOSRC $LOCALREPO
-# else
-# ansible-galaxy install -r ~/.config/workstation/requirements.yml
-
 git clone -b "$GITBRANCH" "$REPOSRC" "$LOCALREPO" 2> /dev/null || git -C "$LOCALREPO" pull
 ansible-galaxy install -r "$LOCALREPO/requirements.yml"
 ansible-playbook -b "$LOCALREPO/playbook.yml"
